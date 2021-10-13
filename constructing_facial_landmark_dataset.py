066b6a05ac417df3163d677633db2d033f8ebe9f
evan pan <evan12345pan@hotmail.com>
2021-10-13 16:28:49 -0400

    e

--- constructing_facial_landmark_dataset.py
+++ constructing_facial_landmark_dataset.py
@@ -291,8 +291,8 @@ if __name__ == "__main__":
     show_normalized_pts = False
     tolerance = 0.01
 
-    video_title = ["rolling_in_the_deep_2_jali_more_alignment_fixes.mp4"]
-    video_path = ["C:\\Users\\evansamaa\\Desktop\\Jali_Experiments\\ten_videos\\rolling_in_the_deep"]
+    video_title = ["child_in_time_1_Jali_raw.mp4"]
+    video_path = ["/Volumes/EVAN_DISK/ten_videos/Child_in_time"]
 
     extract_landmarks_media_pipe(video_title[0],
                              video_path[0], save_annotated_video=True)
