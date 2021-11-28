1a95d13e1da4bb11676eaf04aca7d5a638478940
evansamaa <evan12345pan@hotmail.com>
2021-11-28 08:16:30 -0500

    e

--- models/vowel_recognition_nodel.py
+++ models/vowel_recognition_nodel.py
@@ -140,7 +140,7 @@ if __name__ == "__main__":
     # input things
     # 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
     dataset_root = "C:/Users/evansamaa/Desktop/Dataset/"
-    model_name = "viseme_net_model"
+    model_name = "viseme_net_model_larger_model"
     # prepare pytorch stuff
     if torch.cuda.is_available():
         dev = "cuda:0"
@@ -158,11 +158,11 @@ if __name__ == "__main__":
 
     model = LSTM_vowel_recognizer()
     # for re-training
-    model.load_state_dict(torch.load("../../Dataset/viseme_net_model/model_epoch_650.pt")['model_state_dict'])
+    model.load_state_dict(torch.load("../../Dataset/viseme_net_model_larger_model/model_epoch_650.pt")['model_state_dict'])
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.00001)
     # for re-training
-    optimizer.load_state_dict(torch.load("../../Dataset/viseme_net_model/model_epoch_650.pt")['optimizer_state_dict'])
+    optimizer.load_state_dict(torch.load("../../Dataset/viseme_net_model_larger_model/model_epoch_650.pt")['optimizer_state_dict'])
     epochs = 10000
     training_set = Custom_Dataset(os.path.join(dataset_root, os.path.join("train", 'annotations_medusa.csv')), device)
     train_dataloader = DataLoader(training_set, batch_size=512, shuffle=False)
