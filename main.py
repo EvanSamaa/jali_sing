4748ce927b60aae00ceb95984eceb772783c633d
evansamaa <evan12345pan@hotmail.com>
2022-05-05 13:11:38 -0400

    e

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import json
 from util.jali_curve_generation import *
 
 if __name__ == "__main__":
-    dir = "F:/MASC/Jali_sing/validation/faceware session 2022 Apr 1st/julie_takes/rap_jali/"
+    dir = "F:\\MASC\\Jali_sing\\Revision\\validation\\faceware session Yannis\\Oh Canada\\jali_sing_stuff\\"
     # tim = Minimal_song_data_structure(dir+"rap.wav", dir+"rap.txt")
     # tim.compute_self_phoneme_alignment()
     # tim.write_textgrid(dir, "rap_alignment")
-    j = JaliVoCa_animation(dir + "rap.wav", dir + "rap_alignment.TextGrid", dir + "jali_MVP.json")
+    j = JaliVoCa_animation(dir + "Ps0_facecam.wav", dir + "Ps1_facecam.TextGrid", dir + "jali_curves_MVP.json")
     j.generate_curves()
